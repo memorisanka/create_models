6d9fecf276c1d257b8a6c1f102e775b529bff602
memorisanka <j.krawczyk87@gmail.com>
2022-11-26 16:54:51 +0100

    feat(customers/models): create Product and Card models

--- customers/models.py
+++ customers/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 
-class Customer(models.Model):
-    name = models.CharField('Customer', max_length=30)
-    registration_date = models.DateTimeField(auto_now_add=True)
-    # Cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
-
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
 
 class Cart(models.Model):
-    product = models.CharField(max_length=50)
-    quantity = models.IntegerField()
+    product = models.ForeignKey(Product, blank=True, null=True, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+
+class Customer(models.Model):
+    name = models.CharField("Customer", max_length=30)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE, auto_created=True)
+
+    def __str__(self):
+        return self.name
+
